6b0754fbc6c9e019e26a7c4fdca59fb2c8ac7f01
Farhaan Mukarram <farhaanmukarram@gmail.com>
2023-07-30 14:56:12 +0500

    Fix styling for large screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import scarecrowUrl from "./assets/scarecrow.png";
 
 const App = () => {
   return (
-    <>
-      <main className="flex flex-col px-6 py-5 mx-auto xl:px-20 xl:py-10 max-w-7xl">
-        <h1 className="text-2xl font-bold font-inconsolata">404 NOT FOUND</h1>
-        <section className="flex flex-col my-16 xl:flex-row xl:my-52 xl:space-x-52 space-y-[3.75rem]">
+    <div className="flex flex-col justify-between min-h-screen px-6 py-5 mx-auto xl:px-20 xl:py-10 max-w-7xl">
+      <p className="text-2xl font-bold font-inconsolata">404 NOT FOUND</p>
+
+      <main className="flex flex-col">
+        <section className="flex flex-col my-16 xl:flex-row xl:my-52 xl:space-x-52 space-y-[3.75rem] 2xl:my-auto">
           <img
             src={scarecrowUrl}
             alt="scarecrow image"
@@ -13,9 +14,10 @@ const App = () => {
           />
           <div className="flex flex-col space-y-16">
             <div className="space-y-8 flex-flex-col xl:space-y-9">
-              <h2 className="xl:text-[4rem] leading-normal text-5xl font-bold">
+              <p className="xl:text-[4rem] leading-normal text-5xl font-bold">
                 I have bad news for you
-              </h2>
+              </p>
+
               <p className="text-lg leading-tight xl:text-2xl">
                 The page you are looking for might be removed or is temporarily
                 unavailable
@@ -30,12 +32,12 @@ const App = () => {
       </main>
 
       <footer>
-        <p className="font-medium text-center font-montserrat text-[#BDBDBD] mb-6">
+        <p className="font-medium text-center font-montserrat text-[#BDBDBD]">
           created by <span className="font-bold">farhaan-mukarram</span> -
           devChallenges.io
         </p>
       </footer>
-    </>
+    </div>
   );
 };
 
